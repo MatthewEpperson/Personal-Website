d645fc816958827f155ed286a696e7e69a47a8f0
MatthewEpperson <87238803+MatthewEpperson@users.noreply.github.com>
2021-07-27 21:15:05 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ $('#aboutBtn').click(function(e){
     var $target = $('html, #about');
     $target.animate({scrollTop: 975}, 600);
 });
+
+$('#portfolioBtn').click(function(e){
+    var $target = $('html, #portfolio');
+    $target.animate({scrollTop: 2000}, 600);
+});
