1e7d5bce571934f51586db03fefdd76f4a2170ba
MatthewEpperson <87238803+MatthewEpperson@users.noreply.github.com>
2021-07-29 13:51:19 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -3,7 +3,22 @@ $('#aboutBtn').click(function(e){
     $target.animate({scrollTop: 975}, 600);
 });
 
+$('#experienceBtn').click(function(e){
+    var $target = $('html, #experience');
+    $target.animate({scrollTop: 1600}, 600);
+});
+
+$('#educationBtn').click(function(e){
+    var $target = $('html, #education');
+    $target.animate({scrollTop: 2270}, 600);
+});
+
 $('#portfolioBtn').click(function(e){
     var $target = $('html, #portfolio');
-    $target.animate({scrollTop: 1875}, 600);
+    $target.animate({scrollTop: 3270}, 600);
+});
+
+$('#contactBtn').click(function(e){
+    var $target = $('html, #contact');
+    $target.animate({scrollTop: 5000}, 600);
 });
